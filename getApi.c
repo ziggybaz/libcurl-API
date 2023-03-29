14cfd168f5d2338cd97c4e0b95de196750c40baa
ziggybaz <77249316+ziggybaz@users.noreply.github.com>
2023-03-29 19:41:14 +0300

    Update getApi.c

--- getApi.c
+++ getApi.c
@@ -4,7 +4,22 @@
 
 int main (void)
 {
-  curl_global_init();
+  CURL *curl;
+  CURLcode response;
+  
+  curl_global_init(CURL_GLOBAL_ALL);
+  
+  curl = curl_easy_init();
+  if(curl){
+    curl_easy_setopt(curl, CURLOPT_URL, "eneter your http url here for posting");
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "enter data to be posted here");
+    response = curl_easy_perform(curl);
+    
+    if(response != CURLE_OK)
+      fprintf(stderr, "Sorry your post request failed: %s\n", curl_easy_stderror(response));
+    
+    curl_easy_cleanup(curl);
+    }
   
   curl_global_cleanup();
   return 0;
